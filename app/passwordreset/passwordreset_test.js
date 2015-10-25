d74c3ced9061b0be686c01deecaa2caa79754535
Alon <alonpoudel@yahoo.com>
2015-10-25 18:22:01 -0500

    Initial passwordreset_test page commit

--- app/passwordreset/passwordreset_test.js
+++ app/passwordreset/passwordreset_test.js
@@ -1,3 +1,19 @@
 /**
  * Created by alonp on 10/25/2015.
  */
+'use strict';
+
+describe('myApp.passwordreset module', function() {
+
+    beforeEach(module('myApp.passwordreset'));
+
+    describe('passwordreset controller', function(){
+
+        it('should ....', inject(function($controller) {
+            //spec body
+            var passwordresetCtrl = $controller('PasswordResetCtrl');
+            expect(passwordresetCtrl).toBeDefined();
+        }));
+
+    });
+});
